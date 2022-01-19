2a6f9d5d54740592d8909d6ca07c79347efc316c
huase84 <2569787692@qq.com>
2022-01-19 16:05:08 +0800

    删除

--- meStudy/src/main/java/com/meStudy/api/user/mapper/UserMapper.java
+++
@@ -1,6 +0,0 @@
-package com.meStudy.api.user.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.meStudy.api.user.entity.User;
-
-public interface UserMapper extends BaseMapper<User> {}
--- meStudy/src/main/java/com/meStudy/api/user/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.meStudy.api.user.service;
-
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.meStudy.api.user.entity.User;
-import com.meStudy.api.user.mapper.UserMapper;
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserService extends ServiceImpl<UserMapper, User> {}
